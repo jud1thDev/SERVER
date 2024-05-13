44ad4b66d76543094ab33c232cd77d5465a0acea
seohyun-lee <applemint2003@gmail.com>
2024-05-13 17:08:37 +0900

    feat: MemberService 생성, Member 조회 임시 로직 추가 및 예외처리

--- src/main/java/com/efub/leadtoyproject/domain/member/service/MemberService.java
+++ src/main/java/com/efub/leadtoyproject/domain/member/service/MemberService.java
@@ -2,6 +2,8 @@ package com.efub.leadtoyproject.domain.member.service;
 
 import com.efub.leadtoyproject.domain.member.domain.Member;
 import com.efub.leadtoyproject.domain.member.repository.MemberRepository;
+import com.efub.leadtoyproject.global.exception.CustomException;
+import com.efub.leadtoyproject.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,12 +14,21 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
     private final MemberRepository memberRepository;
 
-    public Long getCurrentMemberId() {
-        return 1L;
+    // (임시) 현재 회원 객체 가져오기
+    public Member getCurrentMember() {
+        Member member = Member.builder()
+                .memberId(1L)
+                .email("testuser@gmail.com")
+                .accessToken("TestAccessToken")
+                .refreshToken("TestRefreshToken")
+                .build();
+        return member;
+        // return valMember(member.getMemberId()); - TODO: 추후 Auth 개발시 이 부분 로직 변경 예정
     }
 
-    public Member getCurrentMember() {
-        return memberRepository.findByMemberId(getCurrentMemberId())
-                .orElseThrow();
+    @Transactional(readOnly = true)
+    public Member findMemberById(Long id) {
+        return memberRepository.findByMemberId(id)
+                .orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
     }
 }
--- src/main/java/com/efub/leadtoyproject/global/exception/ErrorCode.java
+++ src/main/java/com/efub/leadtoyproject/global/exception/ErrorCode.java
@@ -11,10 +11,11 @@ public enum ErrorCode {
 
     // Default
     ERROR(400, "요청 처리에 실패했습니다."),
+    ENTITY_NOT_FOUND(404, "해당 엔티티를 찾을 수 없습니다."),
 
     // Member
-    ENTITY_NOT_FOUND(404, "요청하신 ID와 일치하는 객체가 존재하지 않습니다."),
-    PERMISSION_REJECTED_USER(403, "삭제 권한이 없는 사용자입니다.")
+    MEMBER_NOT_FOUND(404, "해당 사용자를 찾을 수 없습니다."),
+
     ;
 
     private final int status;
