798ba820938d63ae75acaf7f05b7bb39ec7c69f2
seohyun-lee <applemint2003@gmail.com>
2024-05-13 13:02:43 +0900

    feat: 스프링 프로젝트 생성

---
+++ src/test/java/com/efub/leadtoyproject/LeadToyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efub.leadtoyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadToyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
