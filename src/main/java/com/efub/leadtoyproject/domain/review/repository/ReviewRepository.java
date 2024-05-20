c42eb98838791faff60667d26438e1ecec500727
judith <8w8u8@naver.com>
2024-05-20 15:22:43 +0900

    merge: feat/product, feat/review

--- src/main/java/com/efub/leadtoyproject/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/efub/leadtoyproject/domain/review/repository/ReviewRepository.java
@@ -3,7 +3,5 @@ package com.efub.leadtoyproject.domain.review.repository;
 import com.efub.leadtoyproject.domain.review.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 }
