a1962f6f716ae0b549f14ce3a2421101ae0872eb
judith <8w8u8@naver.com>
2024-05-22 19:14:03 +0900

    merge: develop, feat/review

--- src/main/java/com/efub/leadtoyproject/global/config/S3Config.java
+++ src/main/java/com/efub/leadtoyproject/global/config/S3Config.java
@@ -1,4 +1,4 @@
-package com.efub.leadtoyproject.config;
+package com.efub.leadtoyproject.global.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
